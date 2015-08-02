97e154af7743f74f19f382b80c27af1b0682f384
Andre Luckow <andre.luckow@gmail.com>
2015-08-02 13:16:59 -0400

    spark data generator

--- util/init_spark.py
+++ util/init_spark.py
@@ -1,7 +1,11 @@
 import os, sys, time
 
-SPARK_HOME="/home1/01131/tg804093/src/supercomputing2015-tutorial/02_hadoop_on_hpc/work/spark-1.4.1-bin-hadoop2.6"
-os.environ["SPARK_HOME"]=SPARK_HOME
+if not os.environ.has_key("SPARK_HOME"):
+    SPARK_HOME="/home1/01131/tg804093/src/supercomputing2015-tutorial/02_hadoop_on_hpc/work/spark-1.4.1-bin-hadoop2.6"    
+    os.environ["SPARK_HOME"]=SPARK_HOME
+else:
+    SPARK_HOME=os.environ["SPARK_HOME"]
+    
 sys.path.insert(0, os.path.join(SPARK_HOME, "python"))
 sys.path.insert(0, os.path.join(SPARK_HOME, 'python/lib/py4j-0.8.2.1-src.zip')) 
 sys.path.insert(0, os.path.join(SPARK_HOME, 'bin') )
