27f0af97529589862cb971d06006d4a6bcebc313
Andre Luckow <andre.luckow@gmail.com>
2015-08-08 10:55:36 -0400

    removed config file

--- env.py
+++
@@ -1,4 +0,0 @@
-import os
-
-#os.environ["SPARK_HOME"]="/home1/01131/tg804093/src/supercomputing2015-tutorial/02_hadoop_on_hpc/work/spark-1.4.1-bin-hadoop2.6"
-os.environ["SPARK_HOME"]="/usr/local/Cellar/apache-spark/1.4.1/libexec"
