0db250e6ff6d1bf2d7a3aedb3c811f23075ace88
Andre Luckow <Andre.Luckow@gmail.com>
2015-08-31 00:30:26 +0000

    Working on notebooks for video

--- env.py
+++ env.py
@@ -1,3 +1,6 @@
 import os
 
-os.environ["SPARK_HOME"]="/home1/01131/tg804093/src/supercomputing2015-tutorial/02_hadoop_on_hpc/work/spark-1.4.1-bin-hadoop2.6"
+
+os.environ["SPARK_HOME"]="/usr/hdp/2.3.0.0-2557/spark/"
+
+#os.environ["SPARK_HOME"]="/home1/01131/tg804093/src/supercomputing2015-tutorial/02_hadoop_on_hpc/work/spark-1.4.1-bin-hadoop2.6"
