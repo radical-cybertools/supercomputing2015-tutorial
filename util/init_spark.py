b9b27d7697074d95f1b72ce1ccc8335bebed9288
Andre Luckow <andre.luckow@gmail.com>
2015-11-08 03:04:00 +0000

    adjusted paths for AWS

--- util/init_spark.py
+++ util/init_spark.py
@@ -1,7 +1,7 @@
 import os, sys, time
 
 if not os.environ.has_key("SPARK_HOME"):
-    SPARK_HOME="/home1/01131/tg804093/src/supercomputing2015-tutorial/02_hadoop_on_hpc/work/spark-1.4.1-bin-hadoop2.6"    
+    SPARK_HOME="/usr/hdp/2.3.2.0-2950/spark-1.5.1-bin-hadoop2.6/"    
     os.environ["SPARK_HOME"]=SPARK_HOME
 else:
     SPARK_HOME=os.environ["SPARK_HOME"]
@@ -9,7 +9,7 @@ else:
 
 #print "Init Spark: " + SPARK_HOME
 
-os.environ["PYSPARK_PYTHON"]="/opt/anaconda/envs/python2/bin/python"
+os.environ["PYSPARK_PYTHON"]="/opt/anaconda/bin/python"
 os.environ["PYSPARK_DRIVER_PYTHON"]="ipython"
 os.environ["PYSPARK_DRIVER_PYTHON_OPTS"]="notebook"
 os.environ["PYTHONPATH"]= os.path.join(SPARK_HOME, "python")+":" + os.path.join(SPARK_HOME, "python/lib/py4j-0.8.2.1-src.zip")
