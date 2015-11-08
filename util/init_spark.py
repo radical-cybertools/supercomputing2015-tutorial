c33f39b457beb1b1b4c513beabf0121422a16ce7
Andre Luckow <andre.luckow@gmail.com>
2015-11-08 16:41:59 +0000

    updated Spark and MapReduce Example

--- util/init_spark.py
+++ util/init_spark.py
@@ -23,3 +23,22 @@ from pyspark import SparkContext, SparkConf, Accumulator, AccumulatorParam
 from pyspark.sql import SQLContext
 from pyspark.sql.types import *
 from pyspark.mllib.linalg import Vector
+
+import pandas as pd
+from IPython.display import HTML
+
+YARN_HOST="http://yarn-aws.radical-cybertools.org:8088"
+
+def print_application_url(yarn_output):
+    applications=[]
+    for line in yarn_output:
+        comp = line.split('\t')
+        if comp[0].startswith("application"):
+            user=comp[3].strip()
+            url=YARN_HOST+"/proxy/" + comp[0].strip()
+            application_url="<a target=blank href='"+url+"'>"+url+"</a>"
+            applications.append((user, comp[1], application_url))
+
+    pd.set_option('max_colwidth', 1000)
+    df=pd.DataFrame(applications, columns=["User", "Name", "Spark Application URL"])
+    return HTML(df.to_html(escape=False))
