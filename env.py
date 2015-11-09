93e159a3a19dd9f2b7e1fa96ae4600c91f32bdc2
Andre Luckow <andre.luckow@gmail.com>
2015-11-09 02:40:24 +0000

    phone call with Shantenu

---
+++ env.py
@@ -0,0 +1,4 @@
+import os
+
+#os.environ["SPARK_HOME"]="/home1/01131/tg804093/src/supercomputing2015-tutorial/02_hadoop_on_hpc/work/spark-1.4.1-bin-hadoop2.6"
+os.environ["SPARK_HOME"]="/usr/hdp/2.3.2.0-2950/spark-1.5.1-bin-hadoop2.6"
